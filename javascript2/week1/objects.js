714e04d3386c1f5c17d34526c17a2a7fbce26837
Tõnis Rand <16854594+fn-ix@users.noreply.github.com>
2022-05-24 19:10:53 +0200

    Add files via upload

---
+++ javascript2/week1/objects.js
@@ -0,0 +1,88 @@
+const body = document.querySelector('body');
+
+const header = document.createElement('header');
+body.appendChild(header);
+
+const heading = document.createElement('h1');
+heading.innerText = "Breaking Bad character page";
+header.appendChild(heading);
+
+const headingCaptionLink = document.createElement('a');
+headingCaptionLink.innerText = "Breaking Bad API";
+headingCaptionLink.setAttribute('href', 'https://breakingbadapi.com/');
+const headingCaption = document.createElement('p');
+headingCaption.innerText = 'The page displays a random selection of 8 characters from the show; refresh the page to get another selection.\nBuilt with pure JavaScript & CSS, and the ';
+header.appendChild(headingCaption);
+headingCaption.appendChild(headingCaptionLink);
+
+const refreshButton = document.createElement('button');
+refreshButton.innerText = '🗘';
+header.appendChild(refreshButton);
+refreshButton.addEventListener("click", refresh);
+function refresh() {
+  location.reload();
+}
+
+function card(apidata) {
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+
+  const charName = document.createElement('h3');
+  const charImg = document.createElement('img');
+  const charOccupation = document.createElement('p');
+
+  const hidden = document.createElement('div');
+  const charNick = document.createElement('p');
+  const charBorn = document.createElement('p');
+  const charStatus = document.createElement('p');
+  const charActor = document.createElement('p');
+
+  charName.innerText = apidata.name;
+  charImg.setAttribute('src', apidata.img);
+  charOccupation.innerText = apidata.occupation[0];
+  charOccupation.setAttribute('class', 'caption');
+
+  hidden.setAttribute('class', 'hidden');
+  charNick.innerText = apidata.nickname;
+  apidata['birthday'] === 'Unknown' ? charBorn.innerText = 'Unknown' : charBorn.innerText = apidata['birthday'].substring(6, 10);
+  charStatus.innerText = apidata.status;
+  charActor.innerText = apidata.portrayed;
+
+  body.appendChild(card);
+
+  card.appendChild(charName);
+  card.appendChild(charImg);
+  card.appendChild(charOccupation);
+
+  card.appendChild(hidden);
+  hidden.appendChild(charNick);
+  hidden.appendChild(charBorn);
+  hidden.appendChild(charStatus);
+  hidden.appendChild(charActor);
+};
+
+function idSelect(apidata) {
+  let select = [];
+  let index;
+
+  here: while (select.length < 8) {
+    index = Math.floor(Math.random() * apidata.length);
+    for (let id of select) {
+      if (id === index) {
+        continue here;
+      }
+    }
+    select.push(index);
+  }
+  return select;
+}
+
+fetch("https://www.breakingbadapi.com/api/characters")
+  .then(res => res.json())
+  .then(data => {
+    let select = idSelect(data);
+
+    for (let id of select) {
+      card(data[id]);
+    }
+  });
