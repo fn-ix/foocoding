ada26722a3a457bf773d064a71faef10345d3ada
Tõnis Rand <16854594+fn-ix@users.noreply.github.com>
2022-05-25 17:22:19 +0200

    Add files via upload

--- javascript2/week1/objects.js
+++ javascript2/week1/objects.js
@@ -1,4 +1,5 @@
 const head = document.querySelector('head');
+
 const title = document.createElement('title');
 title.innerText = 'Breaking Bad character page';
 head.appendChild(title);
@@ -8,58 +9,71 @@ const body = document.querySelector('body');
 const header = document.createElement('header');
 body.appendChild(header);
 
-const heading = document.createElement('h1');
-heading.innerText = "Breaking Bad character page";
+const heading = document.createElement('div');
+heading.setAttribute('class', 'heading');
 header.appendChild(heading);
+
+const headingTitle = document.createElement('h1');
+headingTitle.innerText = "Breaking Bad character page";
+heading.appendChild(headingTitle);
 
 const headingCaptionLink = document.createElement('a');
 headingCaptionLink.innerText = "Breaking Bad API";
 headingCaptionLink.setAttribute('href', 'https://breakingbadapi.com/');
+
 const headingCaption = document.createElement('p');
 headingCaption.innerText = 'The page displays a random selection of 8 characters from the show; hitting the refresh button to the right will load another selection.\nBuilt with pure JavaScript & CSS, and the ';
-header.appendChild(headingCaption);
+heading.appendChild(headingCaption);
 headingCaption.appendChild(headingCaptionLink);
+
+const searchBox = document.createElement('input');
+header.appendChild(searchBox);
 
 const refreshButton = document.createElement('button');
 refreshButton.innerText = '🗘';
 header.appendChild(refreshButton);
 
-
 function card(apidata) {
   const card = document.createElement('div');
   card.setAttribute('class', 'card');
-
-  const charName = document.createElement('h3');
-  const charImg = document.createElement('img');
-  const charOccupation = document.createElement('p');
-
-  const hidden = document.createElement('div');
-  const charNick = document.createElement('p');
-  const charBorn = document.createElement('p');
-  const charStatus = document.createElement('p');
-  const charActor = document.createElement('p');
-
-  charName.innerText = apidata.name;
-  charImg.setAttribute('src', apidata.img);
-  apidata.occupation[0] === 'unknown' ? charOccupation.innerText = 'Occupation unknown' : charOccupation.innerText = apidata.occupation[0];
-  charOccupation.setAttribute('class', 'caption');
-
-  hidden.setAttribute('class', 'hidden');
-  charNick.innerText = apidata.nickname;
-  apidata['birthday'] === 'Unknown' ? charBorn.innerText = 'Unknown' : charBorn.innerText = apidata['birthday'].substring(6, 10);
-  charStatus.innerText = apidata.status;
-  charActor.innerText = apidata.portrayed;
-
   body.appendChild(card);
 
+  const charName = document.createElement('h3');
+  charName.innerText = apidata.name;
   card.appendChild(charName);
+
+  const charImg = document.createElement('img');
+  charImg.setAttribute('src', apidata.img);
   card.appendChild(charImg);
+
+  const charOccupation = document.createElement('p');
+  apidata.occupation[0] === 'unknown' ? charOccupation.innerText = 'Occupation unknown' : charOccupation.innerText = apidata.occupation[0];
+  charOccupation.setAttribute('class', 'caption');
   card.appendChild(charOccupation);
 
+  const hidden = document.createElement('div');
+  hidden.setAttribute('class', 'hidden');
   card.appendChild(hidden);
+
+  const hRule = document.createElement('hr');
+
+  const charNick = document.createElement('p');
+  charNick.innerText = apidata.nickname;
   hidden.appendChild(charNick);
+  charNick.after(hRule.cloneNode(true));
+
+  const charBorn = document.createElement('p');
+  apidata['birthday'] === 'Unknown' ? charBorn.innerText = 'Unknown' : charBorn.innerText = apidata['birthday'].substring(6, 10);
   hidden.appendChild(charBorn);
+  charBorn.after(hRule.cloneNode(true));
+
+  const charStatus = document.createElement('p');
+  charStatus.innerText = apidata.status;
   hidden.appendChild(charStatus);
+  charStatus.after(hRule.cloneNode(true));
+
+  const charActor = document.createElement('p');
+  charActor.innerText = apidata.portrayed;
   hidden.appendChild(charActor);
 };
 
@@ -79,22 +93,43 @@ function idSelect(apidata) {
   return select;
 }
 
-fetch("https://www.breakingbadapi.com/api/characters")
+function delCards() {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => card.remove());
+}
+
+function loadCards(apidata, select) {
+  delCards();
+  for (let id of select) {
+    card(apidata[id]);
+  }
+}
+
+function searchCards(apidata) {
+  delCards();
+  let result = [];
+
+  for (character of apidata) {
+    if (character.name.toLowerCase().includes(searchBox.value.toLowerCase())) {
+      result.push(apidata.indexOf(character));
+    }
+  }
+  loadCards(apidata, result);
+}
+
+fetch('https://www.breakingbadapi.com/api/characters')
   .then(res => res.json())
   .then(data => {
-    let select = idSelect(data);
+    loadCards(data, idSelect(data));
+    const reCard = function () {
+      loadCards(data, idSelect(data));
+      searchBox.value = '';
+    };
+    refreshButton.addEventListener('click', reCard);
 
-    for (let id of select) {
-      card(data[id]);
-    }
-
-    function reCard() {
-      const cards = document.querySelectorAll('.card');
-      cards.forEach(card => card.remove());
-      select = idSelect(data);
-      for (let id of select) {
-        card(data[id]);
-      }
-    }
-    refreshButton.addEventListener("click", reCard);
-  });
+    const searchResult = function () {
+      searchCards(data);
+    };
+    searchBox.addEventListener('input', searchResult);
+  }
+  );
