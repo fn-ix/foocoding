73f29bfcd58a80765cd9797fa4fad02391fbaa4e
Tõnis Rand <16854594+fn-ix@users.noreply.github.com>
2022-05-28 17:44:40 +0200

    Add files via upload

--- javascript2/week1/objects.js
+++ javascript2/week1/objects.js
@@ -31,7 +31,7 @@ searchBox.setAttribute('placeholder', 'Filter by name...');
 header.appendChild(searchBox);
 
 const refreshButton = document.createElement('button');
-refreshButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="30" height="30" x="0" y="0" viewBox="0 0 94.073 94.072" style="enable-background:new 0 0 30 30" xml:space="preserve" class=""><g>
+refreshButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="30" x="0" y="0" viewBox="0 0 94.073 94.072" style="enable-background:new 0 0 30 30" xml:space="preserve" class=""><g>
 <g xmlns="http://www.w3.org/2000/svg">
 	<g>
 		<path d="M91.465,5.491c-0.748-0.311-1.609-0.139-2.18,0.434l-8.316,8.316C72.046,5.057,60.125,0,47.399,0    c-2.692,0-5.407,0.235-8.068,0.697C21.218,3.845,6.542,17.405,1.944,35.244c-0.155,0.599-0.023,1.235,0.355,1.724    c0.379,0.489,0.962,0.775,1.581,0.775h12.738c0.839,0,1.59-0.524,1.878-1.313c3.729-10.193,12.992-17.971,23.598-19.814    c1.747-0.303,3.525-0.456,5.288-0.456c8.428,0,16.299,3.374,22.168,9.5l-8.445,8.444c-0.571,0.572-0.742,1.432-0.434,2.179    c0.311,0.748,1.039,1.235,1.848,1.235h28.181c1.104,0,2-0.896,2-2V7.338C92.7,6.53,92.211,5.801,91.465,5.491z" fill="#008080" data-original="#008080" class=""></path>
