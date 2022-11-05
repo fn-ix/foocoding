7add6adcfb6a5972f7726a1329ed3425f0feab87
Tõnis Rand <tonis.rand@tutanota.com>
2022-11-06 00:22:08 +0100

    remove console logs

--- database/week1/node-server/index.js
+++ database/week1/node-server/index.js
@@ -14,8 +14,6 @@ db.connect((err) => {
   if (err) {
     throw err;
   }
-
-  console.log('MySQL connected');
 });
 
 // Create express server
@@ -68,6 +66,4 @@ app.get('/:id', (req, res) => {
 });
 
 // Run server
-app.listen('3000', () => {
-  console.log('Server started on port 3000');
-});
+app.listen('3000');
